f27f89942ec986ad3d1972abc3e30a81a743cf27
hawnzug <hawnzug@gmail.com>
2017-02-21 09:34:49 +0800

    Add some tests

--- test/CoreSpec.hs
+++ test/CoreSpec.hs
@@ -5,9 +5,36 @@ import Test.Hspec
 import Core
 import Parser
 
+alpha s1 s2 = case parseExpr s1 of
+                Right e1 -> case parseExpr s2 of
+                              Right e2 -> alphaEq e1 e2
+beta s1 s2 = case parseExpr s1 of
+               Right e1 -> case parseExpr s2 of
+                             Right e2 -> betaEq emptyContext e1 e2
+
 spec :: Spec
-spec = describe "freeVars" $ do
-    it "something" $
-        freeVars (parseExpr "x") `shouldBe` ["x"]
-    it "something" $
-        freeVars (parseExpr "fun x : N => x") `shouldBe` ["N"]
+spec = do
+    describe "alphaEq" $ do
+        it "x == x" $
+            alpha "x" "x" `shouldBe` True
+        it "x != y" $
+            alpha "x" "y" `shouldBe` False
+        it "\\x : y . x == \\a : t . a" $
+            alpha "fun x : y => x" "fun a : y => a" `shouldBe` True
+        it "(\\x : y -> y . x)(\\x : y . x) == (\\a : y -> y . a)(\\b : y . b)" $
+            alpha "(fun x : forall u : y -> y => x) (fun x : y => x)"
+                  "(fun a : forall u : y -> y => a) (fun b : y => b)"
+                  `shouldBe` True
+        it "\\a : Type 0 . a == \\b : Type 0 . b" $
+            alpha "fun a : Type 0 => a"
+                  "fun b : Type 0 => b"
+                  `shouldBe` True
+    describe "betaEq" $ do
+        it "(\\x : Type 0 . \\x : x . x) y == \\x : y . x" $
+            beta "(fun x : Type 0 => fun x : x => x) y"
+                 "fun x : y => x"
+                 `shouldBe` True
+        it "(\\A : Type 0 . \\x : A . x) N z == z" $
+            beta "(fun A : Type 0 => fun x : A => x) N z"
+                 "z"
+                 `shouldBe` True
